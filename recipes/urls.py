8c210671386dab54b63676706472a823750f797d
bull0n <lucas.bulloni@protonmail.ch>
2019-11-30 09:37:15 +0100

    view set for recipe, still need one for ingredient

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'recipes', views.RecipeViewSet)
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include(router.urls)),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rest_framework import viewsets
+
+from .serializers import RecipeSerializer
+from .models import Recipe
+
 # Create your views here.
 def index(request):
     return HttpResponse("Test")
 
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
