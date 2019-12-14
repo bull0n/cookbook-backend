bea3f56a7ef663712c1d87e95afa37b6f4b61eb8
bull0n <lucas.bulloni@protonmail.ch>
2019-12-14 23:11:14 +0100

    get the recipe ingredients with their quantity

--- recipes/graphql_definitions/query.py
+++ recipes/graphql_definitions/query.py
@@ -9,13 +9,11 @@ class Query(object):
     recipe = graphene.Field(RecipeType, id=graphene.Int())
     recipes = graphene.List(RecipeType)
     steps = graphene.List(StepType)
+    ingredients = graphene.List(IngredientType)
 
     def resolve_recipe(self, info, recipe_id):
         return Recipe.objects.get(id=recipe_id)
 
     def resolve_recipes(self, info, **kwargs):
         return Recipe.objects.all()
-
-    def resolve_steps(self, info, **kwargs):
-        return Step.objects.all()
     
--- recipes/graphql_definitions/types.py
+++ recipes/graphql_definitions/types.py
@@ -1,15 +1,26 @@
-
+import graphene
 from graphene_django.types import DjangoObjectType
 
 from ..models import Ingredient, Recipe, Step
 
+class IngredientType(DjangoObjectType):
+    quantity = graphene.Float()
+    
+    def resolve_quantity(instance, info):
+        if hasattr(info.context, 'recipe_id'):
+            recipe_id = info.context.recipe_id
+            relation = instance.recipecontainsingredient_set.get(recipe=Recipe.objects.get(id=recipe_id))
+            return relation.quantity
+
+    class Meta:
+        model = Ingredient
+
 class RecipeType(DjangoObjectType):
     class Meta:
         model = Recipe
-
-class IngredientType(DjangoObjectType):
-    class Meta:
-        model = Ingredient
+    
+    def resolve_ingredients(instance, info):
+        info.context.recipe_id = instance.id
 
 class StepType(DjangoObjectType): 
     def resolve_duration(self, info):
